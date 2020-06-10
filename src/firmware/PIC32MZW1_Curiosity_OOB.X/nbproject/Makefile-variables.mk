cb491f0b74fc8b88efb5fa905b4051111aaec5b5
Vysakh Pillai <vysakh.pillai@microchip.com>
2020-06-10 13:00:54 +0530

    include missed project files

---
+++ src/firmware/PIC32MZW1_Curiosity_OOB.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# pic32mz_w1_curiosity configuration
+CND_ARTIFACT_DIR_pic32mz_w1_curiosity=dist/pic32mz_w1_curiosity/production
+CND_ARTIFACT_NAME_pic32mz_w1_curiosity=PIC32MZW1_Curiosity_OOB.X.production.hex
+CND_ARTIFACT_PATH_pic32mz_w1_curiosity=dist/pic32mz_w1_curiosity/production/PIC32MZW1_Curiosity_OOB.X.production.hex
+CND_PACKAGE_DIR_pic32mz_w1_curiosity=${CND_DISTDIR}/pic32mz_w1_curiosity/package
+CND_PACKAGE_NAME_pic32mz_w1_curiosity=pic32mzw1curiosityoob.x.tar
+CND_PACKAGE_PATH_pic32mz_w1_curiosity=${CND_DISTDIR}/pic32mz_w1_curiosity/package/pic32mzw1curiosityoob.x.tar
