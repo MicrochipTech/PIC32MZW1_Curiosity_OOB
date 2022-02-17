8c97fabb9bdf0102ec441da7ce07dbd9af166147
Pranjal Das <pranjal.das@microchip.com>
2022-02-17 13:20:41 +0530

    updating "prebuilt" folder

---
+++ src/prebuilt/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# pic32mz_w1_curiosity configuration
+CND_ARTIFACT_DIR_pic32mz_w1_curiosity=dist/pic32mz_w1_curiosity/production
+CND_ARTIFACT_NAME_pic32mz_w1_curiosity=PIC32MZW1_Curiosity_OOB.X.production.hex
+CND_ARTIFACT_PATH_pic32mz_w1_curiosity=dist/pic32mz_w1_curiosity/production/PIC32MZW1_Curiosity_OOB.X.production.hex
